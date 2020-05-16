38f7056ae9c8b264a4e36ab4b916347f6ef821b7
Olga <krasolga2018@gmail.com>
2020-05-16 07:10:12 +0400

    commit

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import { PositionsService } from 'src/app/shared/services/positions.service';
-import { Position } from 'src/app/shared/interfaces';
-import { MaterialService, MaterialInstance } from 'src/app/shared/classes/material.service';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core'
+import {PositionsService} from '../../../shared/services/positions.service'
+import {Position} from '../../../shared/interfaces'
+import {MaterialInstance, MaterialService} from '../../../shared/classes/material.service'
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-positions-form',
@@ -11,43 +12,109 @@ import { MaterialService, MaterialInstance } from 'src/app/shared/classes/materi
 export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input('categoryId') categoryId: string
   @ViewChild('modal') modalRef: ElementRef
+
   positions: Position[] = []
   loading = false
+  positionId = null
   modal: MaterialInstance
+  form: FormGroup
 
   constructor(private positionsService: PositionsService) {
   }
 
   ngOnInit() {
-    //Начало загрузки списка услуг
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      cost: new FormControl(1, [Validators.required, Validators.min(1)])
+    })
+
     this.loading = true
-    //Получение списка всех услуг, относящихся к текущему мастеру
-    this.positionsService.fetch(this.categoryId)
-      .subscribe(positions => {
-        this.positions = positions
-        // Окончание загрузки списка услуг
-        this.loading = false
-      })
+    this.positionsService.fetch(this.categoryId).subscribe(positions => {
+      this.positions = positions
+      this.loading = false
+    })
   }
 
   ngOnDestroy() {
     this.modal.destroy()
   }
 
-  // Генерация модального окна
   ngAfterViewInit() {
     this.modal = MaterialService.initModal(this.modalRef)
   }
 
   onSelectPosition(position: Position) {
+    this.positionId = position._id
+    this.form.patchValue({
+      name: position.name,
+      cost: position.cost
+    })
     this.modal.open()
+    MaterialService.updateTextInputs()
   }
 
-  onAddPosition(){
+  onAddPosition() {
+    this.positionId = null
+    this.form.reset({name: null, cost: 1})
     this.modal.open()
+    MaterialService.updateTextInputs()
   }
 
-  onCancel(){
+  onDeletePosition(event: Event, position: Position) {
+    event.stopPropagation()
+    const decision = window.confirm(`Удалить позицию "${position.name}"?`)
+
+    if (decision) {
+      this.positionsService.delete(position).subscribe(
+        response => {
+          const idx = this.positions.findIndex(p => p._id === position._id)
+          this.positions.splice(idx, 1)
+          MaterialService.toast(response.message)
+        },
+        error => MaterialService.toast(error.error.message)
+      )
+    }
+  }
+
+  onCancel() {
     this.modal.close()
+  }
+
+  onSubmit() {
+    this.form.disable()
+
+    const newPosition: Position = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    }
+
+    const completed = () => {
+      this.modal.close()
+      this.form.reset({name: '', cost: 1})
+      this.form.enable()
+    }
+
+    if (this.positionId) {
+      newPosition._id = this.positionId
+      this.positionsService.update(newPosition).subscribe(
+        position => {
+          const idx = this.positions.findIndex(p => p._id === position._id)
+          this.positions[idx] = position
+          MaterialService.toast('Изменения сохранены')
+        },
+        error => MaterialService.toast(error.error.message),
+        completed
+      )
+    } else {
+      this.positionsService.create(newPosition).subscribe(
+        position => {
+          MaterialService.toast('Позиция создана')
+          this.positions.push(position)
+        },
+        error => MaterialService.toast(error.error.message),
+        completed
+      )
+    }
   }
 }
